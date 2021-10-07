d88bc45558a37e16a9948dfa2750aa223e0572c9
muskarab <mustafa.kamal.mk858@gmail.com>
2021-10-07 20:52:04 +0700

    update route api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\AuthorController;
+use App\Http\Controllers\Api\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
