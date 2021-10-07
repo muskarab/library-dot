3ffcbb502e8808f11914859f126bebd1170d0c16
muskarab <mustafa.kamal.mk858@gmail.com>
2021-10-07 20:39:47 +0700

    add BookController and Book model

---
+++ database/migrations/2021_10_07_133807_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id');
+            $table->string('title')->unique();
+            $table->string('slug');
+            $table->string('img');
+            $table->longText('content');
+            $table->bigInteger('page');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
