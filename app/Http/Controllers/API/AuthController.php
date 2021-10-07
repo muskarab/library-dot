99ec0f758e8ea4d0fbe70250dc8592e651de7cc2
muskarab <mustafa.kamal.mk858@gmail.com>
2021-10-07 20:37:10 +0700

    add AuthController

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        } else {
+            $token = $user->createToken('token')->plainTextToken;
+            return response()->json([
+                'message' => 'Password atau Email Sesuai',
+                'user' => $user,
+                'token' => $token
+            ], 200);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Berhasil logout',
+        ], 200);
+    }
+}
