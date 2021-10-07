ae145d6a0759056e1b8a402d7ee068904ebb3d84
muskarab <mustafa.kamal.mk858@gmail.com>
2021-10-07 20:40:15 +0700

    edit book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,5 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
-    
+    protected $guarded = ['id'];
+    protected $with = ['author'];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['title'] ?? false, function ($query, $search) {
+            return $query->where('title', 'like', '%' . $search . '%');
+        });
+
+        $query->when($filters['author'] ?? false, function ($query, $author) {
+            return $query->whereHas('author', function ($query) use ($author) {
+                $query->where('slug', $author);
+            });
+        });
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
