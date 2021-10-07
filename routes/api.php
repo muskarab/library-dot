f991bacd248362f359360669eebbcb328c3cc015
muskarab <mustafa.kamal.mk858@gmail.com>
2021-10-07 20:32:10 +0700

    add route login and logout with AuthController

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/logout', [AuthController::class, 'logout']);
 });
+Route::post('/login', [AuthController::class, 'login']);
